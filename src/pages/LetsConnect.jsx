9c0cc8d9c1badc57ba1b393db76b4312e9dff2eb
HarryMolla <hmiol5577@gmail.com>
2025-09-11 17:08:39 +0300

    UI update for lets connect page

--- src/pages/LetsConnect.jsx
+++ src/pages/LetsConnect.jsx
@@ -24,7 +24,7 @@ export default function CallToAction() {
           <img
             src={image}
             alt="Harry"
-            className="md:w-70 md:h-70 w-40 h-40 rounded-full relative z-10  md:mt-0 -mt-30"
+            className="md:w-70 md:h-70 w-40 h-40 rounded-full relative z-10  md:-mt-10 -mt-30"
           />
           <div className="absolute inset-0 flex justify-center items-center pointer-events-none overflow-hidden">
             <Ripple
@@ -34,11 +34,11 @@ export default function CallToAction() {
             />
           </div>
         </div>
-        <p className="font-bold text-center text-gray-600 text-2xl md:mb-5 -mt-40">
+        <p className="font-bold text-center text-gray-600 text-2xl md:mb-5  -mt-40">
           Hiryakos Molla
         </p>
         <GradientText
-          className="text-2xl font-medium text-center mr-5 ml-5 md:mb-3 md:-mt-10 -mt-12"
+          className="text-2xl font-medium text-center mr-5 ml-5 md:mb-3 md:-mt-15 -mt-12"
           text="UI/UX Designer & Front-End Developer"
           neon
           gradient="linear-gradient(90deg, #00ff00 0%, #00ffff 25%, #ff00ff 50%, #00ffff 75%, #00ff00 100%)"
@@ -55,12 +55,12 @@ export default function CallToAction() {
             initial={{ scale: 0.8 }}
             animate={{ scale: 1 }}
             transition={{ duration: 0.5 }}
-            className="text-2xl font-semibold text-gray-500 text-center"
+            className=" font-semibold text-gray-500 text-center"
           >
             Let's Connect
           </motion.h2>
 
-          <div className="grid gap-3 w-full">
+          <div className="grid gap-2 w-full">
             <button
               onClick={handleEmailMe}
               className="border-2 border-blue-500 bg-blue-100 text-blue-500 hover:text-white py-3 rounded-md font-semibold transition-colors duration-300 hover:bg-blue-500 hover:shadow-lg flex-1 flex items-center justify-center gap-2"
